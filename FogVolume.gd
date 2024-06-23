3df2c94b0611330843a1b1a29a70edfffedfec80
Marcel B <adsci81@gmail.com>
2024-06-23 14:44:10 +0200

    Update to 4.2.2 and added sub surface effects + changed default water values

---
+++ FogVolume.gd
@@ -0,0 +1,8 @@
+extends FogVolume
+
+@export var visible_height_offset := 0.7
+
+func _process(_delta):
+	var camera = get_viewport().get_camera_3d()
+	if camera:
+		visible = camera.position.y < (position.y + visible_height_offset)
